2d8a9711adf1d370f14dcf2d8960487b0b9a25e8
Weilin Wang <144287229+A004772214@users.noreply.github.com>
2023-11-26 14:10:27 -0400

    Update AboutMe.js

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,7 +6,7 @@ function AboutMe() {
     <div>
       <img src={imgURL } />
       <h1>Weilin(William) Wang</h1>
-      <p>Hi, I am Weilin(William) Wang, I have a Master Degree in Electrical Engineering from New Jersey Institute of Technology in US. Over my 9 years's professional career, I have been carried out different roles such as sofeware developer, data analyst, bussiness analyst and project manager. In addition, I have worked in sofeware, pharmaceutical and financial industries. My skill set is throught the whole process of SDLC, from front-end to back-end. Thus I can handle differnet projects and different roles in the industry.</p>
+      <p>This is production enviroment. Hi, I am Weilin(William) Wang, I have a Master Degree in Electrical Engineering from New Jersey Institute of Technology in US. Over my 9 years's professional career, I have been carried out different roles such as sofeware developer, data analyst, bussiness analyst and project manager. In addition, I have worked in sofeware, pharmaceutical and financial industries. My skill set is throught the whole process of SDLC, from front-end to back-end. Thus I can handle differnet projects and different roles in the industry.</p>
       <p>The reason why i chose MCDA program is because I like the program structure, it covers all the skills we might need in the industry and even have the co-op terms. Also, I can learn from everyone in our cohort because everyone has several years' industry experience in different domains. I believe that's gonna help me a lot in the future job market.</p>
     </div>
   );
